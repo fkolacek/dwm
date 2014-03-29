24be5a6a2f9799c99b181719ee9d187c6276abe7
Frantisek Kolacek <work@kolacek.it>
2014-03-29 22:11:45 +0100

    Changed shortcuts: launch browser via Win, volume up/down via alt+up/down (avoiding known bug with fn volume keys mapping)

--- config.h
+++ config.h
@@ -62,20 +62,24 @@ static const Layout layouts[] = {
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, "-l", "10", NULL };
 static const char *termcmd[]  = { "gnome-terminal", NULL };
+static const char *browsercmd[]  = { "google-chrome", NULL };
 static const char *filemanagercmd[]  = { "terminator", "-e", "ranger", NULL };
 static const char *cmdkilldwm[]  = { "killall", "dwm", NULL };
 /* Sound stuff */
 #define KEY_SOUND_UP 0x1008ff13
 #define KEY_SOUND_DOWN 0x1008ff11
 #define KEY_SOUND_TOGGLE 0x1008ff12
+/* Custom keys */
 #define KEY_PRINT 0xff61
+#define KEY_WIN 0xffeb
+
 static const char *cmdsoundup[]  = { "amixer", "-q", "sset", "Master", "5%+", NULL };
 static const char *cmdsounddown[]  = { "amixer", "-q", "sset", "Master", "5%-", NULL };
 static const char *cmdsoundtoggle[]  = { "amixer", "-q", "sset", "Master", "toggle", NULL };
 
 
-static const char *cmdKB[]  = { "switch-keyboard.sh", NULL, NULL };
 /* Scripts */
+static const char *cmdKB[]  = { "switch-keyboard.sh", NULL, NULL };
 static const char *cmdopenbrowser[]  = { "open-browser.sh", NULL };
 static const char *cmddwmmenu[]  = { "dwm-menu.sh", NULL };
 static const char *cmdscreenshot[]  = { "screenshot", "-i", NULL };
@@ -86,6 +90,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ 0,             KEY_PRINT, spawn,          {.v = termcmd } },
+	{ 0,             KEY_WIN, spawn,          {.v = browsercmd } },
 	{ MODKEY|ShiftMask,             XK_f,      spawn,          {.v = filemanagercmd} },
 	{ MODKEY,             XK_c,      spawn,          {.v = cmdopenbrowser } },
 	{ MODKEY|ShiftMask,   XK_m,      spawn,          {.v = cmddwmmenu } },
@@ -93,8 +98,10 @@ static Key keys[] = {
 	{ MODKEY,   XK_q,      spawn,          {.v = cmdKB } },
 	{ MODKEY|ShiftMask,   XK_s,      spawn,          {.v = cmdscreenshot } },
 	/* Sound */
-	{ 0,                            KEY_SOUND_UP,     spawn,         {.v = cmdsoundup } },
-	{ 0,                            KEY_SOUND_DOWN,   spawn,         {.v = cmdsounddown } },
+	{ MODKEY,                            XK_Up,     spawn,         {.v = cmdsoundup } },
+	//{ 0,                            KEY_SOUND_UP,     spawn,         {.v = cmdsoundup } },
+	{ MODKEY,                            XK_Down,   spawn,         {.v = cmdsounddown } },
+	//{ 0,                            KEY_SOUND_DOWN,   spawn,         {.v = cmdsounddown } },
 	{ 0,                            KEY_SOUND_TOGGLE, spawn,         {.v = cmdsoundtoggle } },
 	/* Killing dwm */
 	{ MODKEY|ShiftMask,             XK_q,      spawn,           {.v = cmdkilldwm} },
